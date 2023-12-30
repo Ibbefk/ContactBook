41f5bd00ae16949bd46db69b87517c63dea00414
Ibbefk <143652215+Ibbefk@users.noreply.github.com>
2023-12-30 04:39:46 +0100

    Add files via upload

---
+++ Crito/ConsoleApp/ContactBook.cs
@@ -0,0 +1,83 @@
+﻿// ContactBook.cs
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+
+public class ContactBook
+{
+    private List<Contact> contacts;
+    private string filePath = "contacts.json";
+
+    public ContactBook()
+    {
+        LoadContacts();
+    }
+
+    public void AddContact(Contact contact)
+    {
+        contacts.Add(contact);
+        SaveContacts();
+        Console.WriteLine($"Contact {contact.FirstName} {contact.LastName} added successfully.");
+    }
+
+    public void RemoveContact(string email)
+    {
+        Contact contactToRemove = contacts.Find(c => c.Email == email);
+        if (contactToRemove != null)
+        {
+            contacts.Remove(contactToRemove);
+            SaveContacts();
+            Console.WriteLine($"Contact {contactToRemove.FirstName} {contactToRemove.LastName} removed successfully.");
+        }
+        else
+        {
+            Console.WriteLine("Contact not found.");
+        }
+    }
+
+    public void ListContacts()
+    {
+        Console.WriteLine("Contact List:");
+        foreach (var contact in contacts)
+        {
+            Console.WriteLine($"{contact.FirstName} {contact.LastName}, {contact.Email}");
+        }
+    }
+
+    public void DisplayContactDetails(string email)
+    {
+        Contact contact = contacts.Find(c => c.Email == email);
+        if (contact != null)
+        {
+            Console.WriteLine($"Name: {contact.FirstName} {contact.LastName}");
+            Console.WriteLine($"Phone: {contact.PhoneNumber}");
+            Console.WriteLine($"Email: {contact.Email}");
+            Console.WriteLine($"Address: {contact.Address}");
+            Console.WriteLine($"Additional Info: Nickname - {contact.Nickname}, Birthday - {contact.Birthday}");
+        }
+        else
+        {
+            Console.WriteLine("Contact not found.");
+        }
+    }
+
+    public void LoadContacts()
+    {
+        if (File.Exists(filePath))
+        {
+            string json = File.ReadAllText(filePath);
+            contacts = JsonConvert.DeserializeObject<List<Contact>>(json);
+        }
+        else
+        {
+            contacts = new List<Contact>();
+        }
+    }
+
+    public void SaveContacts()
+    {
+        string json = JsonConvert.SerializeObject(contacts, Formatting.Indented);
+        File.WriteAllText(filePath, json);
+    }
+}
---
+++ Crito/ConsoleApp/Program.cs
@@ -0,0 +1,96 @@
+﻿// Program.cs
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome To My Contact Book App!");
+
+        ContactBook contactBook = new ContactBook();
+
+        while (true)
+        {
+            Console.WriteLine("1. Add Contact");
+            Console.WriteLine("2. List Contacts");
+            Console.WriteLine("3. Show Contact Details");
+            Console.WriteLine("4. Remove Contact");
+            Console.WriteLine("5. Exit");
+
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    AddContact(contactBook);
+                    break;
+                case "2":
+                    ListContacts(contactBook);
+                    break;
+                case "3":
+                    DisplayContactDetails(contactBook);
+                    break;
+                case "4":
+                    RemoveContact(contactBook);
+                    break;
+                case "5":
+                    contactBook.SaveContacts();
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("Please try again, invalid choice.");
+                    break;
+            }
+        }
+    }
+
+    static void AddContact(ContactBook contactBook)
+    {
+        Console.Write("Enter First Name: ");
+        string firstName = Console.ReadLine();
+        Console.Write("Enter Last Name: ");
+        string lastName = Console.ReadLine();
+        Console.Write("Enter Phone Number: ");
+        string phoneNumber = Console.ReadLine();
+        Console.Write("Enter Email: ");
+        string email = Console.ReadLine();
+        Console.Write("Enter Address: ");
+        string address = Console.ReadLine();
+
+        // Additional personal touch
+        Console.Write("Enter Nickname (optional): ");
+        string nickname = Console.ReadLine();
+        Console.Write("Enter Birthday (optional): ");
+        string birthday = Console.ReadLine();
+
+        Contact contact = new Contact
+        {
+            FirstName = firstName,
+            LastName = lastName,
+            PhoneNumber = phoneNumber,
+            Email = email,
+            Address = address,
+            Nickname = nickname,
+            Birthday = birthday
+        };
+
+        contactBook.AddContact(contact);
+    }
+
+    static void ListContacts(ContactBook contactBook)
+    {
+        contactBook.ListContacts();
+    }
+
+    static void DisplayContactDetails(ContactBook contactBook)
+    {
+        Console.Write("Enter Email of the contact: ");
+        string email = Console.ReadLine();
+        contactBook.DisplayContactDetails(email);
+    }
+
+    static void RemoveContact(ContactBook contactBook)
+    {
+        Console.Write("Enter Email of the contact to remove: ");
+        string email = Console.ReadLine();
+        contactBook.RemoveContact(email);
+    }
+}
